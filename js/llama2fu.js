a6b5be25c5e81a66a3580defbeac346ced240519
Piotr Fusik <piotr@fusion-lang.org>
2026-05-06 21:28:03 +0200

    JavaScript port.

---
+++ js/llama2fu.js
@@ -0,0 +1,61 @@
+// llama2fu.js - Llama 2 LLM command-line interface 
+// Copyright (c) 2026 Piotr Fusik
+// SPDX-License-Identifier: MIT
+
+import fs from "fs";
+import { Loader, Llama2Cli } from "./llama2cli.js";
+
+class NodeLoader extends Loader
+{
+	#f
+
+	open(path)
+	{
+		this.#f = fs.openSync(path);
+	}
+
+	#read(a)
+	{
+		if (fs.readSync(this.#f, a) != a.length * a.BYTES_PER_ELEMENT)
+			throw new Error();
+	}
+
+	readInt()
+	{
+		const a = new Int32Array(1);
+		this.#read(a);
+		return a[0];
+	}
+
+	readFloat()
+	{
+		const a = new Float32Array(1);
+		this.#read(a);
+		return a[0];
+	}
+
+	readWeights(a, n)
+	{
+		this.#read(a);
+	}
+
+	skipBytes(n)
+	{
+		this.#read(new Uint8Array(n));
+	}
+
+	readString()
+	{
+		const b = new Uint8Array(this.readInt());
+		this.#read(b);
+		return new TextDecoder().decode(b);
+	}
+
+	close()
+	{
+		fs.closeSync(this.#f);
+	}
+}
+
+const args = process.argv.slice(2);
+process.exit(Llama2Cli.run(args, args.length, new NodeLoader()));
